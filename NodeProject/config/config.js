21fc8b9c97df19662d8311e3a3f281ac5e3b4319
JormmungandM <75366379+JormmungandM@users.noreply.github.com>
2022-10-24 10:03:16 +0300

    Delete config.js

--- NodeProject/config/config.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-    secret_key:
-      "a3c8104e05932eef7302033f79243072d394f2f95fe2bb234a67cfe78f35ba3af335a6cf7d4e94d85323a72bf487cc429ccb918772f60e3961e9ff65f3dbdeef",
-  };
