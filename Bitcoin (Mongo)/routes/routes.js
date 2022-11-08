297e159d6cb20b3674152ddf8690871ed736d658
JormmungandM <75366379+JormmungandM@users.noreply.github.com>
2022-11-08 20:28:24 +0200

    Bitcoin 'routes.js' added comments

--- Bitcoin (Mongo)/routes/routes.js
+++ Bitcoin (Mongo)/routes/routes.js
@@ -9,8 +9,9 @@ const router = new Router();
 router.route("/bitcoin")
 .get((req, res) => {
 
+    // получаем данные о биткоине
     request("https://api.coindesk.com/v1/bpi/currentprice.json", function(error, response, body) {
-    const bitcoin = JSON.parse(body);
+    const bitcoin = JSON.parse(body); // преобразуем строку данных в объект bitcoin
     const {EUR, USD, GBP} = bitcoin.bpi;
 
     // инфо в консоль 
@@ -19,16 +20,14 @@ router.route("/bitcoin")
     console.log("Bitcoin. " + USD.code + " -> " + USD.rate)
     console.log("Bitcoin. " + GBP.code + " -> " + GBP.rate)
     
-
+    // подключаемся и сохраняем
     mongoose.connect(config.mongo)
         .then(()=>{
             console.log("Connected...");
-            //EUR
-            start(EUR.code, EUR.rate, bitcoin.time.updated)
-            //USD
-            start(USD.code, USD.rate, bitcoin.time.updated)
-            //GBP
-            start(GBP.code, GBP.rate, bitcoin.time.updated)
+            
+            start(EUR.code, EUR.rate, bitcoin.time.updated)     // EUR
+            start(USD.code, USD.rate, bitcoin.time.updated)     // USD 
+            start(GBP.code, GBP.rate, bitcoin.time.updated)     // GBP
         })
         .catch((err)=>{
             console.log(err);
